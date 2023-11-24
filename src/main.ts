5c61d4c85dee6d9cae4bf558e345a39b774b2565
wolfdev1 <wolfdev1@outlook.com>
2023-11-24 22:13:20 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('bootstrap');
+  const logger = new Logger('Main');
   logger.log(`Application listening on port ${process.env.PORT || 3000}`);
   await app.listen(process.env.PORT || 3000);
 }
